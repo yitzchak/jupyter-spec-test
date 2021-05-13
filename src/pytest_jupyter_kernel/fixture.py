24bd0171eec92fb12279c0b59be346c667187c14
Tarn W. Burton <twburton@gmail.com>
2021-05-13 06:47:58 -0400

    Refactor

--- src/pytest_jupyter_kernel/fixture.py
+++ src/pytest_jupyter_kernel/fixture.py
@@ -20,12 +20,13 @@ class Kernel(object):
         self.kernel.start_kernel()
         self.client = self.kernel.client()
         self.client.start_channels()
+        self.client.wait_for_ready()
 
     def validate_message(self, msg, source):
         msg_type = msg['header']['msg_type']
         self.message_validator.validate(msg)
-        if msg["parent_header"] is not None and "msg_id" in msg["parent_header"]:
-            assert msg["parent_header"]["msg_id"] in self.pending, "Unknown parent message id."
+        #if msg["parent_header"] is not None and "msg_id" in msg["parent_header"]:
+        #    assert msg["parent_header"]["msg_id"] in self.pending, "Unknown parent message id."
 
     def read_replies(self, timeout = None, stdin_hook = None,
                      keep_status = False):
@@ -66,45 +67,53 @@ class Kernel(object):
             if stdin_socket in events:
                 msg = self.client.get_stdin_msg()
                 self.validate_message(msg, 'stdin')
-                assert stdin_hook is not None, "Input request received but no hook available."
-                stdin_hook(msg)
+                if ("msg_id" in msg["parent_header"]
+                    and msg["parent_header"]["msg_id"] in self.pending):
+                    assert stdin_hook is not None, "Input request received but no hook available."
+                    stdin_hook(msg)
             if shell_socket in events:
                 msg = self.client.get_shell_msg()
                 self.validate_message(msg, 'shell')
-                parent_msg_id = msg["parent_header"]["msg_id"]
-                assert self.pending[parent_msg_id] == "shell", "Received response on shell channel for a control message."
-                replies[parent_msg_id] = msg
-                if parent_msg_id in idle:
-                    del self.pending[parent_msg_id]
+                if ("msg_id" in msg["parent_header"]
+                    and msg["parent_header"]["msg_id"] in self.pending):
+                    parent_msg_id = msg["parent_header"]["msg_id"]
+                    assert self.pending[parent_msg_id] == "shell", "Received response on shell channel for a control message."
+                    replies[parent_msg_id] = msg
+                    if parent_msg_id in idle:
+                        del self.pending[parent_msg_id]
             if control_socket in events:
                 msg = self.client.get_control_msg()
                 self.validate_message(msg, 'control')
-                parent_msg_id = msg["parent_header"]["msg_id"]
-                assert self.pending[parent_msg_id] == "control", "Received response on control channel for a shell message."
-                replies[parent_msg_id] = msg
-                if parent_msg_id in idle:
-                    del self.pending[parent_msg_id]
+                if ("msg_id" in msg["parent_header"]
+                    and msg["parent_header"]["msg_id"] in self.pending):
+                    parent_msg_id = msg["parent_header"]["msg_id"]
+                    assert self.pending[parent_msg_id] == "control", "Received response on control channel for a shell message."
+                    replies[parent_msg_id] = msg
+                    if parent_msg_id in idle:
+                        del self.pending[parent_msg_id]
             if iopub_socket in events:
                 msg = self.client.get_iopub_msg()
                 self.validate_message(msg, 'iopub')
-                if msg["parent_header"] is None or "msg_id" not in msg["parent_header"]:
-                    continue
-                parent_msg_id = msg["parent_header"]["msg_id"]
-                if parent_msg_id not in messages:
-                    assert (msg["header"]["msg_type"] == "status"
-                        and msg["content"]["execution_state"] == "busy")
-                    messages[parent_msg_id] = [msg] if keep_status else []
-                elif (
-                    msg["header"]["msg_type"] == "status"
-                    and msg["content"]["execution_state"] == "idle"
-                ):
-                    idle[parent_msg_id] = True
-                    if parent_msg_id in replies:
-                        del self.pending[parent_msg_id]
-                    if keep_status:
+                if ("msg_id" in msg["parent_header"]
+                    and msg["parent_header"]["msg_id"] in self.pending):
+                    if msg["parent_header"] is None or "msg_id" not in msg["parent_header"]:
+                        continue
+                    parent_msg_id = msg["parent_header"]["msg_id"]
+                    if parent_msg_id not in messages:
+                        assert (msg["header"]["msg_type"] == "status"
+                            and msg["content"]["execution_state"] == "busy")
+                        messages[parent_msg_id] = [msg] if keep_status else []
+                    elif (
+                        msg["header"]["msg_type"] == "status"
+                        and msg["content"]["execution_state"] == "idle"
+                    ):
+                        idle[parent_msg_id] = True
+                        if parent_msg_id in replies:
+                            del self.pending[parent_msg_id]
+                        if keep_status:
+                            messages[parent_msg_id].append(msg)
+                    else:
                         messages[parent_msg_id].append(msg)
-                else:
-                    messages[parent_msg_id].append(msg)
 
         return replies, messages
 
@@ -165,9 +174,10 @@ class Kernel(object):
     def input(self, string):
         self.client.input(string)
 
-    def test_execute(self, code, silent = False, store_history = True,
-        user_expressions = None, stop_on_error = True,
-        timeout = None, stdin_hook = None, keep_status = False):
+    def execute_read_reply(self, code, silent = False, store_history = True,
+                           user_expressions = None, stop_on_error = True,
+                           timeout = None, stdin_hook = None,
+                           keep_status = False):
         msg_id = self.execute(code, silent = silent,
                               store_history = store_history,
                               user_expressions = user_expressions,
@@ -180,7 +190,7 @@ class Kernel(object):
         assert any(msg['msg_type'] == 'execute_input' and msg['content']['code'] == code for msg in messages[msg_id])
         return replies[msg_id], messages[msg_id] if msg_id in messages else []
 
-    def test_complete(self, code, cursor_pos = None, timeout = None):
+    def complete_read_reply(self, code, cursor_pos = None, timeout = None):
         msg_id = self.complete(code, cursor_pos = cursor_pos)
         replies, messages = self.read_replies(timeout = timeout)
         assert (len(replies) == 1 and msg_id in replies
@@ -188,7 +198,7 @@ class Kernel(object):
                 and replies[msg_id]["content"]["status"] == "ok")
         return replies[msg_id], messages[msg_id] if msg_id in messages else []
 
-    def test_inspect(self, code, cursor_pos = None, detail_level = 0,
+    def inspect_read_reply(self, code, cursor_pos = None, detail_level = 0,
                      timeout = None):
         msg_id = self.inspect(code, cursor_pos = cursor_pos,
                               detail_level = detail_level)
@@ -198,7 +208,7 @@ class Kernel(object):
                 and replies[msg_id]["content"]["status"] == "ok")
         return replies[msg_id], messages[msg_id] if msg_id in messages else []
 
-    def test_history(self, raw = True, output = False, timeout = None,
+    def history_read_reply(self, raw = True, output = False, timeout = None,
                      hist_access_type = "range", **kwargs):
         msg_id = self.history(raw = raw, output = output,
                               hist_access_type = hist_access_type,
@@ -209,7 +219,7 @@ class Kernel(object):
                 and replies[msg_id]["content"]["status"] == "ok")
         return replies[msg_id], messages[msg_id] if msg_id in messages else []
 
-    def test_kernel_info(self, timeout = None):
+    def kernel_info_read_reply(self, timeout = None):
         msg_id = self.kernel_info()
         replies, messages = self.read_replies(timeout = timeout)
         assert (len(replies) == 1 and msg_id in replies
@@ -217,7 +227,7 @@ class Kernel(object):
                 and replies[msg_id]["content"]["status"] == "ok")
         return replies[msg_id], messages[msg_id] if msg_id in messages else []
 
-    def test_comm_info(self, target_name = None, timeout = None):
+    def comm_info_read_reply(self, target_name = None, timeout = None):
         msg_id = self.comm_info(target_name = target_name)
         replies, messages = self.read_replies(timeout = timeout)
         assert (len(replies) == 1 and msg_id in replies
@@ -225,7 +235,7 @@ class Kernel(object):
                 and replies[msg_id]["content"]["status"] == "ok")
         return replies[msg_id], messages[msg_id] if msg_id in messages else []
 
-    def test_is_complete(self, code, timeout = None):
+    def is_complete_read_reply(self, code, timeout = None):
         msg_id = self.is_complete(code)
         replies, messages = self.read_replies(timeout = timeout)
         assert (len(replies) == 1 and msg_id in replies
@@ -242,23 +252,23 @@ def jupyter_kernel(request):
 
 
 def test_execute(jupyter_kernel):
-    reply, messages = jupyter_kernel.test_execute("(1+ 7)", timeout = 10)
+    reply, messages = jupyter_kernel.execute_read_reply("(1+ 7)", timeout = 10)
     assert any(msg['msg_type'] == 'execute_result' and msg['content']['data']['text/plain'] == '8' for msg in messages), "wibble"
 
 
 def test_kernel_info(jupyter_kernel):
-    reply, messages = jupyter_kernel.test_kernel_info(timeout = 10)
+    reply, messages = jupyter_kernel.kernel_info_read_reply(timeout = 10)
     assert reply['content']['implementation'] == 'common-lisp'
 
 
 def test_comm_info(jupyter_kernel):
-    reply, messages = jupyter_kernel.test_comm_info(timeout = 10)
+    reply, messages = jupyter_kernel.comm_info_read_reply(timeout = 10)
 
 
 def test_is_complete(jupyter_kernel):
-    reply, messages = jupyter_kernel.test_is_complete("(fu bar)", timeout = 10)
+    reply, messages = jupyter_kernel.is_complete_read_reply("(fu bar)", timeout = 10)
     assert reply['content']['status'] == 'complete'
 
-    reply, messages = jupyter_kernel.test_is_complete("(fu bar", timeout = 10)
+    reply, messages = jupyter_kernel.is_complete_read_reply("(fu bar", timeout = 10)
     assert reply['content']['status'] == 'incomplete'
 
